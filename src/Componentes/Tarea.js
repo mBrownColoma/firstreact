5931a040cf571227e42483471206aa04ce1f9ce0
m.brown <m.brown@miguelbrown.cl>
2022-07-20 15:49:15 -0400

    tareas con estilos

--- src/Componentes/Tarea.js
+++ src/Componentes/Tarea.js
@@ -1,13 +1,29 @@
 import react, {Component} from "react";
 
 class Tarea extends Component{
+    TareaHecha(){
+        return{
+            fontSize:'20px',
+            color: this.props.tarea.estado ? 'gray':'black',
+            textDecoration: this.props.tarea.estado ? 'line-through':'none'
+        }
+    }
     render(){
-        return <div>
+        const btnBorrar = {
+            fontSize: '18px',
+            background: '#263F3B',
+            color: '#F57B3B',
+            border: 'none',
+            padding: '10px 15 px',
+            borderRadius: '50%',
+            cursor: 'pointer'
+        }
+    return <div style={this.TareaHecha()}>
             {this.props.tarea.titulo} -
             {this.props.tarea.descripcion} -
             {this.props.tarea.id}
             <input type="checkbox"></input>
-            <button>X</button> 
+            <button style={btnBorrar}>X</button> 
         </div>
     }
 
