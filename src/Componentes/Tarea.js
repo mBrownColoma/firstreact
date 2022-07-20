be7906b1993b8b8605b60407eff116cfd07ed0be
m.brown <m.brown@miguelbrown.cl>
2022-07-20 15:09:40 -0400

    creacion de componentes a renderisar como tareas

---
+++ src/Componentes/Tarea.js
@@ -0,0 +1,12 @@
+import react, {Component} from "react";
+
+class Tarea extends Component{
+    render(){
+        return <div>
+            {this.props.tarea.}
+        </div>
+    }
+
+}
+
+export default Tarea;
